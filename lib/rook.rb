a6acff66e6c74f61a3c23dee9ffdc8caa2f2010b
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-03-24 13:35:08 -0300

    update

--- lib/rook.rb
+++ lib/rook.rb
@@ -1,28 +1,8 @@
 class Rook < Piece
+    include Slideable
+    
     def to_s
         @color == :black ? "♜ " : "♖ "
-    end
-
-    def available_moves
-        moves = []
-        current_r, current_c = location
-
-        move_dirs.each do |(dr, dc)|
-            current_r, current_c = location
-
-            loop do 
-            current_r += dr
-            current_c += dc
-            loc = [current_r, current_r]
-            break if !board.in_bounds?(loc)
-            if board.empty?(loc)
-                moves << loc
-            end
-            if enemy?(loc)
-                moves << loc
-                break 
-            end
-        end
     end
 
     def move_dirs
